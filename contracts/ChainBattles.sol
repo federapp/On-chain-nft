2dc238dbfc9c444ea6eaff63ee0b2bb64a6983f0
Fede Rapp <federapp@gmail.com>
2022-05-26 16:39:06 +0200

    add attributes and randomness

--- contracts/ChainBattles.sol
+++ contracts/ChainBattles.sol
@@ -11,7 +11,14 @@ contract ChainBattles is ERC721URIStorage {
     using Counters for Counters.Counter; 
     Counters.Counter private _tokenIds;
 
-    mapping(uint256 => uint256) public tokenIdToLevels;
+    struct Attributes {
+        uint256 levels;
+        uint256 speed;
+        uint256 strength;
+        uint256 life;
+    }
+
+    mapping(uint256 => Attributes) public tokenIdToAttributes;
     
     constructor() ERC721 ("Chain Battles", "CBTLS"){
 
@@ -24,6 +31,9 @@ contract ChainBattles is ERC721URIStorage {
             '<rect width="100%" height="100%" fill="black" />',
             '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',"Warrior",'</text>',
             '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">', "Levels: ",getLevels(tokenId),'</text>',
+            '<text x="50%" y="60%" class="base" dominant-baseline="middle" text-anchor="middle">', "Speed: ",getSpeed(tokenId),'</text>',
+            '<text x="50%" y="70%" class="base" dominant-baseline="middle" text-anchor="middle">', "Strength: ",getStrength(tokenId),'</text>',
+            '<text x="50%" y="80%" class="base" dominant-baseline="middle" text-anchor="middle">', "Life: ",getLife(tokenId),'</text>',
             '</svg>'
         );
         return string(
@@ -34,9 +44,31 @@ contract ChainBattles is ERC721URIStorage {
         );
     }
 
+
     function getLevels(uint256 tokenId) public view returns (string memory) {
-        uint256 levels = tokenIdToLevels[tokenId];
+        uint256 levels = tokenIdToAttributes[tokenId].levels;
         return levels.toString();
+    }
+
+    function getLife(uint256 tokenId) public view returns (string memory) {
+        uint256 life = tokenIdToAttributes[tokenId].life;
+        return life.toString();
+    }
+
+    function getSpeed(uint256 tokenId) public view returns (string memory) {
+        uint256 speed = tokenIdToAttributes[tokenId].speed;
+        return speed.toString();
+    }
+
+    function getStrength(uint256 tokenId) public view returns (string memory) {
+        uint256 strength = tokenIdToAttributes[tokenId].strength;
+        return strength.toString();
+    }
+
+
+    uint initialNumber;
+    function randomNumber(uint256 number) public returns(uint256){
+        return uint256(keccak256(abi.encodePacked(initialNumber++))) % number;
     }
 
     function getTokenURI(uint256 tokenId) public returns (string memory){
@@ -59,15 +91,29 @@ contract ChainBattles is ERC721URIStorage {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _safeMint(msg.sender, newItemId);
-        tokenIdToLevels[newItemId] = 0;
+        tokenIdToAttributes[newItemId].levels = 0;
+        tokenIdToAttributes[newItemId].speed = randomNumber(25);
+        tokenIdToAttributes[newItemId].strength = randomNumber(25);
+        tokenIdToAttributes[newItemId].life = randomNumber(25);
         _setTokenURI(newItemId, getTokenURI(newItemId));
     }
 
     function train(uint256 tokenId) public {
         require(_exists(tokenId));
         require(ownerOf(tokenId) == msg.sender,"You must be the owner to train it");
-        uint256 currentLevel = tokenIdToLevels[tokenId];
-        tokenIdToLevels[tokenId] = currentLevel + 1;
+        
+        uint256 currentLevel = tokenIdToAttributes[tokenId].levels;
+        tokenIdToAttributes[tokenId].levels = currentLevel + 1;
+        
+        uint256 currentSpeed = tokenIdToAttributes[tokenId].speed;
+        tokenIdToAttributes[tokenId].speed = currentSpeed + randomNumber(7);
+        
+        uint256 currentLife = tokenIdToAttributes[tokenId].life;
+        tokenIdToAttributes[tokenId].life = currentLife + randomNumber(7);
+        
+        uint256 currentStrength = tokenIdToAttributes[tokenId].strength;
+        tokenIdToAttributes[tokenId].strength = currentStrength + randomNumber(7);
+
         _setTokenURI(tokenId, getTokenURI(tokenId));
     }
 
